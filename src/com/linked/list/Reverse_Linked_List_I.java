2e9308dc1b139a04e8832845db9be166df9bdb23
akhil90s <akhil.chawla2010@gmail.com>
2023-07-08 13:33:44 +0530

    Linked List

--- src/com/linked/list/Reverse_Linked_List_I.java
+++ src/com/linked/list/Reverse_Linked_List_I.java
@@ -2,40 +2,39 @@ package com.linked.list;
 
 public class Reverse_Linked_List_I {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        ListNode node = new ListNode(1);
-        node.next = new ListNode(2);
-        node.next.next = new ListNode(3);
-        node.next.next.next = new ListNode(4);
-        node.next.next.next.next = new ListNode(5);
+		ListNode node = new ListNode(1);
+		node.next = new ListNode(2);
+		node.next.next = new ListNode(3);
+		node.next.next.next = new ListNode(4);
+		node.next.next.next.next = new ListNode(5);
 
-        show(node);
-        ListNode result = reverseList(node);
-        show(result);
+		show(node);
+		ListNode result = reverseList(node);
+		show(result);
 
-    }
+	}
 
-    public static ListNode reverseList(ListNode head) {
-        ListNode curr = head;
-        ListNode prev = null;
-        ListNode next = null;
-        while (curr != null) {
-            next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
-        return prev;
-    }
+	public static ListNode reverseList(ListNode head) {
+		ListNode curr = head;
+		ListNode prev = null;
+		ListNode next = null;
+		while (curr != null) {
+			next = curr.next;
+			curr.next = prev;
+			prev = curr;
+			curr = next;
+		}
+		return prev;
+	}
 
-    public static void show(ListNode head) {
-        while (head != null) {
-            System.out.print(head.val + " -> ");
-            head = head.next;
-        }
-        System.out.println();
-    }
-
+	public static void show(ListNode head) {
+		while (head != null) {
+			System.out.print(head.val + " -> ");
+			head = head.next;
+		}
+		System.out.println();
+	}
 
 }
